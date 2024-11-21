95322ffd988de9bdb4f679c580188ed97988ce0a
CloudSalander <ruben.as532@gmail.com>
2024-11-21 10:43:54 +0100

    Filtering strings for having only letters and numbers

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 
+function filterLettersAndNumbers(string $input): string {
+    return preg_replace('/[^a-zA-Z0-9]/', '', $input);
+}
+
 $text_to_parse = readline();
 var_dump($text_to_parse);
 $parsed_text = trim($text_to_parse);
 var_dump($parsed_text);
 $parsed_text = strtolower($parsed_text);
 var_dump($parsed_text);
+$parsed_text = filterLettersAndNumbers($parsed_text);
+var_dump($parsed_text);
+
+
+?>
+
 
 
 ?>
