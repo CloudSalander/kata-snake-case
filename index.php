94d133b86c166b7d832074f8d466452109263845
CloudSalander <ruben.as532@gmail.com>
2024-11-26 11:07:38 +0100

    First filter, then trim,strtolower..

--- index.php
+++ index.php
@@ -1,26 +1,22 @@
 <?php
-
+//Provar este caso: "·$··"$   hola     ;Munddo
 function filterLettersAndNumbers(string $input): string {
-    return preg_replace('/[^a-zA-Z0-9\s]/', '', $input);
+    return preg_replace('/[^a-zA-Z0-9\s_]/', '', $input);
 }
 
 function filterWhiteSpacesToUnderscore(string $input): string {
+    //This is not coded by Shakespeare,precisely, BUT...
     for($i = 0; $i < strlen($input); ++$i) {
-        if($input[$i] == " " && $input[$i-1] == "_") $input[$i] = str_replace(" ","",$input[$i]);
-        else if($input[$i] == " " && $input[$i-1] != "_") $input[$i] = str_replace(" ","_",$input[$i]);
+        
     }
     return $input;    
 }
 
 $text_to_parse = readline();
-var_dump($text_to_parse);
-$parsed_text = trim($text_to_parse);
-var_dump($parsed_text);
+$parsed_text = filterLettersAndNumbers($text_to_parse);
+$parsed_text = trim($parsed_text);
 $parsed_text = strtolower($parsed_text);
+$parsed_text = filterWhiteSpacesToUnderscore($parsed_text);
 var_dump($parsed_text);
-$parsed_text = filterLettersAndNumbers($parsed_text);
-var_dump($parsed_text);
-//$parsed_text = filterWhiteSpacesToUnderscore($parsed_text);
-//var_dump($parsed_text);
 
 ?>
