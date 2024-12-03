b58fdeb197818b2a314f723ece3efae87659b35e
CloudSalander <ruben.as532@gmail.com>
2024-12-03 18:23:18 +0100

    Apparently working!

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
 <?php
-//Provar este caso: "·$··"$   hola     ;Munddo
+/*Extrem cases: 
+    "·$··"$   hola     ;Munddo
+    "·$··"$   hola     _;Munddo_ 
+*/
 function filterLettersAndNumbers(string $input): string {
     return preg_replace('/[^a-zA-Z0-9\s_]/', '', $input);
 }
 
 function filterWhiteSpacesToUnderscore(string $input): string {
-    //This is not coded by Shakespeare,precisely, BUT...
-    for($i = 0; $i < strlen($input); ++$i) {
-        
-    }
-    return $input;    
+    $input = str_replace(" ","_",$input);
+    $input = preg_replace('/_+/', '_', $input);
+    return $input;
 }
+
 
 $text_to_parse = readline();
 $parsed_text = filterLettersAndNumbers($text_to_parse);
 $parsed_text = trim($parsed_text);
+$parsed_text = trim($parsed_text,"_");
 $parsed_text = strtolower($parsed_text);
 $parsed_text = filterWhiteSpacesToUnderscore($parsed_text);
 var_dump($parsed_text);
